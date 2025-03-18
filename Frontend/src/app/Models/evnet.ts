417b131572f55236895c8d8e5d1b9bac16921f92
OBenAchour <168365209+OBenAchour@users.noreply.github.com>
2025-03-18 21:14:03 +0100

    init frontend

---
+++ Frontend/src/app/Models/evnet.ts
@@ -0,0 +1,9 @@
+// export class event {
+//     id !: number;
+//     titre !: string;
+//     description !: string;
+//     lieu !: string;
+//     date !: string;
+//     disponible !: boolean;
+//     nbrMax !: number;
+// }
