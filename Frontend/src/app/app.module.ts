e68f836588489552e7bdd5472e6713cad5bd9a20
hamdidridi <168366318+hamdidridi@users.noreply.github.com>
2025-03-21 22:11:07 +0100

    Sauvegarde des modifications locales avant la mise à jour

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,19 +4,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import{HttpClientModule} from '@angular/common/http';
-import { AddEventComponent } from './add-event/add-event.component';
-import { ListEventComponent } from './list-event/list-event.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { NotfoundComponent } from './notfound/notfound.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AddEventComponent,
-    ListEventComponent,
-    NavbarComponent,
-    NotfoundComponent
+    
   ],
   imports: [
     BrowserModule,
