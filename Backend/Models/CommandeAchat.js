afab577b3c976d1a88e4b04971aab36ae021b70f
hamdidridi <168366318+hamdidridi@users.noreply.github.com>
2025-03-29 04:07:24 +0100

    update crud commandes

--- Backend/Models/CommandeAchat.js
+++ Backend/Models/CommandeAchat.js
@@ -1,14 +1,18 @@
 // models/CommandeAchat.js
 const mongoose = require('mongoose');
 const CommandeAchatSchema = new mongoose.Schema({
-    produit: { type: mongoose.Schema.Types.ObjectId, ref: 'Produit' },
-    quantite: { type: Number, required: true },
-    prix: { type: Number, required: true },
-    statut: { type: String},
-    fournisseurID: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Fournisseur',
-        required: true
-      }
+  produit: { 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'Produit',  
+      required: true 
+  },
+  quantite: { type: Number},
+  prix: { type: Number, required: true },
+  statut: { type: String, default: "En attente" },
+  fournisseurID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Fournisseur',
+      required: true
+  }
 });
 module.exports = mongoose.model('CommandeAchat', CommandeAchatSchema);
